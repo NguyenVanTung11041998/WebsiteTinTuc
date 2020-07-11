abea0418322373cef95da91d2c35dfd724051ac4
Nguyễn Văn Tùng <vantung11041998nv@outlook.com>
2020-07-11 11:21:13 +0700

    Fix message when error response

--- WebsiteTinTuc.Admin/vue/src/lib/ajax.ts
+++ WebsiteTinTuc.Admin/vue/src/lib/ajax.ts
@@ -6,30 +6,30 @@ const ajax = axios.create({
     timeout: 30000
 });
 ajax.interceptors.request.use(function (config) {
-    if(!!window.abp.auth.getToken()){
-        config.headers.common["Authorization"]="Bearer "+window.abp.auth.getToken();
+    if (!!window.abp.auth.getToken()) {
+        config.headers.common["Authorization"] = "Bearer " + window.abp.auth.getToken();
     }
-    config.headers.common[".AspNetCore.Culture"]=window.abp.utils.getCookieValue("Abp.Localization.CultureName");
-    config.headers.common["Abp.TenantId"]=window.abp.multiTenancy.getTenantIdCookie();
+    config.headers.common[".AspNetCore.Culture"] = window.abp.utils.getCookieValue("Abp.Localization.CultureName");
+    config.headers.common["Abp.TenantId"] = window.abp.multiTenancy.getTenantIdCookie();
     return config;
-  }, function (error) {
-    
+}, function (error) {
+
     return Promise.reject(error);
 });
-let vm=new Vue({});
-ajax.interceptors.response.use((respon)=>{    
+let vm = new Vue({});
+ajax.interceptors.response.use((respon) => {
     return respon
-},(error)=>{
-    if(!!error.response&&!!error.response.data.error&&!!error.response.data.error.message&&error.response.data.error.details){
-        vm.$Modal.error({title:error.response.data.error.message,content:error.response.data.error.details})
-    }else if(!!error.response&&!!error.response.data.error&&!!error.response.data.error.message){
-        vm.$Modal.error({title:window.abp.localization.localize("LoginFailed"),content:error.response.data.error.message})
-    }else if(!error.response){
+}, (error) => {
+    if (!!error.response && !!error.response.data.error && !!error.response.data.error.message && error.response.data.error.details) {
+        vm.$Modal.error({ title: error.response.data.error.message, content: error.response.data.error.details })
+    } else if (!!error.response && !!error.response.data.error && !!error.response.data.error.message) {
+        vm.$Modal.error({ title: window.abp.localization.localize("Thông báo"), content: error.response.data.error.message })
+    } else if (!error.response) {
         vm.$Modal.error(window.abp.localization.localize('UnknownError'));
     }
-    setTimeout(()=>{
-       vm.$Message.destroy();
-    },1000);
+    setTimeout(() => {
+        vm.$Message.destroy();
+    }, 1000);
     return Promise.reject(error);
 })
 export default ajax;
