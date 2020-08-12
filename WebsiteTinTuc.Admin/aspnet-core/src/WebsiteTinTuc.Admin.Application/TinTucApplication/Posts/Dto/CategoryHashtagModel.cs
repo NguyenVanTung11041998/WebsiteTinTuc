190fecc56fe964efb94ba57eb71e9c406cf2b80f
Nguyễn Văn Tùng <tungktpm1@gmail.com>
2020-08-12 07:26:00 +0700

    Fix file gitignore

---
+++ WebsiteTinTuc.Admin/aspnet-core/src/WebsiteTinTuc.Admin.Application/TinTucApplication/Posts/Dto/CategoryHashtagModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebsiteTinTuc.Admin.TinTucApplication.Posts.Dto
+{
+    public class CategoryHashtagModel
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryHashtagOfPostId { get; set; }
+    }
+}
