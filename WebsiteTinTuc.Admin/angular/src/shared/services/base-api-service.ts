082619483b5c0ddd0737ea8641db11e53e79a265
Nguyễn Văn Tùng <tungktpm1@gmail.com>
2022-10-18 21:58:55 +0700

    add service base

---
+++ WebsiteTinTuc.Admin/angular/src/shared/services/base-api-service.ts
@@ -0,0 +1,13 @@
+import { AppConsts } from './../../../shared/AppConsts';
+import { HttpClient } from '@angular/common/http';
+export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
+
+export abstract class BaseApiService {
+    protected baseUrl = AppConsts.remoteServiceBaseUrl;
+
+    protected http: HttpClient;
+
+    constructor(http: HttpClient) {
+        this.http = http;
+    }
+}
